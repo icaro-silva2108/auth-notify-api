79fbe9e7ca082a1013de0cd6e59370f615bbe182
Icaro Silva <icarosilva00562@gmail.com>
2026-07-13 20:05:30 -0300

    feat(security): add AuthenticationProvider and AuthenticationManager beans

--- src/main/java/com/icaro/auth_notify/common/security/config/SecurityConfig.java
+++ src/main/java/com/icaro/auth_notify/common/security/config/SecurityConfig.java
@@ -1,9 +1,15 @@
-package com.icaro.auth_notify.common.config;
+package com.icaro.auth_notify.common.security.config;
+
+import com.icaro.auth_notify.common.security.service.CustomUserDetailsService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +17,23 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider customAuthenticationProvider(
+            CustomUserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
