4f0507cf8892dc9e2961bf8c5d4b552d18c24287
Icaro Silva <icarosilva00562@gmail.com>
2026-07-15 19:35:44 -0300

    feat(security): add JWT service layer

---
+++ src/main/java/com/icaro/auth_notify/common/security/service/JwtService.java
@@ -0,0 +1,79 @@
+package com.icaro.auth_notify.common.security.service;
+
+import com.icaro.auth_notify.user.model.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSingingKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractClaims(String token) {
+
+        return Jwts
+                .parser()
+                .verifyWith(getSingingKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUsername(String token) {
+
+        return extractClaims(token).getSubject();
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(user.getEmail())
+
+                .claims(
+                        Map.of(
+                                "userId", user.getId(),
+                                "role", user.getRole(),
+                                "active", user.isActive()
+                        )
+                )
+
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+
+                .signWith(getSingingKey())
+                .compact();
+    }
+
+    public boolean isTokenExpired(String token) {
+
+        return extractClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+   public boolean isTokenOwnerValid(String token, String email) {
+
+        return extractClaims(token).getSubject().equals(email);
+   }
+}
