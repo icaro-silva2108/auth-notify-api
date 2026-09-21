c34b55ceab70f9fd9f4b61bf6e88340ae42585d8
Icaro Silva <icarosilvadev2108@gmail.com>
2026-09-21 15:01:22 -0300

    chore(database): change the H2 database to PostgreSQL

--- src/test/java/com/icaro/login_system/AuthNotifyApplicationTests.java
+++ src/test/java/com/icaro/login_system/AuthNotifyApplicationTests.java
@@ -1,10 +1,12 @@
 package com.icaro.login_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootConfiguration
 @SpringBootTest
-class LoginSystemApplicationTests {
+class AuthNotifyApplicationTests {
 
 	@Test
 	void contextLoads() {
